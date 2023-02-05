f5670dae3c3fc18d17f5770ce867733d8fbef261
Shivam-Samant <shivamsamant000@gmail.com>
2023-02-05 18:55:01 +0530

    minor changes

--- openCV/readAndSaveImage/index.py
+++ openCV/readAndSaveImage/index.py
@@ -2,11 +2,11 @@ import cv2
 
 # imread
 
-img = cv2.imread('image.png') # reading the image
+img = cv2.imread('image.jpg') # reading the image
 while True:
   cv2.imshow("window", img) # show the image in the window
   if cv2.waitKey(0) == 27: # if we press the escape key(ASCII -> 27) then we break the loop
     break
 
-cv2.imwrite('copyImg.png', img); # save/write the file:  first argument is image name and second is image
+cv2.imwrite('copyImg.jpg', img); # save/write the file:  first argument is image name and second is image
 cv2.destroyAllWindows() # Quit or destroy all windows
